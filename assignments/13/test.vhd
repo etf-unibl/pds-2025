4437000ecb451889cfce623d994546413993ead7
thewantedboss <ana.vulin@student.etf.unibl.org>
2025-10-15 18:59:12 +0200

    Fix test.vhd structure for CI

--- assignments/13/test.vhd
+++ assignments/13/test.vhd
@@ -2,13 +2,9 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 
-entity test is
-
-
-	
-end test;
+entity test is	
+end entity test;
 
 architecture test_arch of test is
-	
-
-end test_arch;
+begin	
+end architecture test_arch;
