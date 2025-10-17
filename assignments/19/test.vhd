df734c233bb336f580265dc3a700a1f375a681aa
AndjelaS997 <164009911+AndjelaS997@users.noreply.github.com>
2025-10-17 19:54:08 +0200

    Issue #19 : Added test.vhd file (#20)

---
+++ assignments/19/test.vhd
@@ -0,0 +1,13 @@
+-- File: test.vhd
+-- Minimalni VHDL dizajn: entitet sa praznom arhitekturom
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity test is
+end entity test;
+
+architecture empty of test is
+begin
+    -- Prazna arhitektura
+end architecture empty;
