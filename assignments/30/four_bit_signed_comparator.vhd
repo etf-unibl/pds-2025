0c0d2ce1f1db8ed2a963aecfe782a603d3dab64c
Andjela Savicic <andjela.savicic@student.etf.unibl.org>
2025-11-10 22:25:07 +0100

    Issue #30 : Create comparator of signed numbers
    - Added two versions of a 4-bit signed comparator:
    - Version 1: Simple implementation using direct comparison
    - Version 2: Manually derived bit-by-bit comparison logic
    - Changed files to meet lint rules

--- assignments/30/four_bit_signed_comparator.vhd
+++ assignments/30/four_bit_signed_comparator.vhd
@@ -1,16 +1,53 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2025
+-- https://github.com/etf-unibl/pds-2025/
+-----------------------------------------------------------------------------
+--
+-- unit name:     four_bit_signed_comparator
+--
+-- description:
+--
+--   4-bit signed comparator (simple compare): outputs '1' when A_i > B_i.
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2025 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2025 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity four_bit_signed_comparator is
-    port (
-			 i_A : in std_logic_vector(3 downto 0);
-          i_B : in std_logic_vector(3 downto 0);
-          o_AGTB : out std_logic
-			 );
-end four_bit_signed_comparator;
+  port (
+    A_i    : in  std_logic_vector(3 downto 0);
+    B_i    : in  std_logic_vector(3 downto 0);
+    AGTB_o : out std_logic
+  );
+end entity four_bit_signed_comparator;
 
-architecture rtl of four_bit_signed_comparator is
+architecture arch of four_bit_signed_comparator is
 begin
-	o_AGTB <= '1' when signed(i_A) > signed (i_B) else '0';
-end rtl;
+  AGTB_o <= '1' when signed(A_i) > signed(B_i) else '0';
+end architecture arch;
