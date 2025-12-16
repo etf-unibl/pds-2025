1e5b436590f0b2ccf6c3dbe7c73564f34212f3a9
Dejan Tomic <dejan.tomic@student.etf.unibl.org>
2025-12-16 17:50:16 +0100

    Issue #51 : Create leading zero counter circuit
    - Modified leading_zero_counter_unit_tb.vhd to fix testbench issue

--- assignments/51/leading_zero_counter_unit_tb.vhd
+++ assignments/51/leading_zero_counter_unit_tb.vhd
@@ -44,39 +44,46 @@ end leading_zero_counter_unit_tb;
 
 architecture arch of leading_zero_counter_unit_tb is
 
-  signal INPUT_DATA_i  : std_logic_vector(15 downto 0);
-  signal OUTPUT_DATA_o : std_logic_vector(4 downto 0);
+  signal INPUT_DATA_i_tb  : std_logic_vector(15 downto 0);
+  signal OUTPUT_DATA_o_tb : std_logic_vector(4 downto 0);
+
+  component leading_zero_counter_unit
+    port (
+      INPUT_DATA_i  : in std_logic_vector(15 downto 0);
+      OUTPUT_DATA_o : out std_logic_vector(4 downto 0)
+    );
+  end component;
 
 begin
 
-  DUT : entity work.leading_zero_counter_unit
+  DUT : leading_zero_counter_unit
     port map (
-      INPUT_DATA_i  => INPUT_DATA_i,
-      OUTPUT_DATA_o => OUTPUT_DATA_o
+      INPUT_DATA_i  => INPUT_DATA_i_tb,
+      OUTPUT_DATA_o => OUTPUT_DATA_o_tb
     );
 
   stim_proc : process
   begin
 
-    INPUT_DATA_i <= "1000000000000000";
+    INPUT_DATA_i_tb <= "1000000000000000";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0100000000000000";
+    INPUT_DATA_i_tb <= "0100000000000000";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0001000000000000";
+    INPUT_DATA_i_tb <= "0001000000000000";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0000000100000000";
+    INPUT_DATA_i_tb <= "0000000100000000";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0000000000000001";
+    INPUT_DATA_i_tb <= "0000000000000001";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0000000000000000";
+    INPUT_DATA_i_tb <= "0000000000000000";
     wait for 10 ns;
 
-    INPUT_DATA_i <= "0010001000100010";
+    INPUT_DATA_i_tb <= "0010001000100010";
     wait for 10 ns;
 
     assert false report "=== SIMULATION COMPLETED ===" severity note;
